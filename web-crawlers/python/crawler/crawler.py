7a896ac04f0c35203386bb3d5b9958afc3dbd01f
Joe Ray <joe@joeray.me>
2024-04-03 22:42:25 +0100

    WIP: Following URLs

--- web-crawlers/python/crawler/crawler.py
+++ web-crawlers/python/crawler/crawler.py
@@ -3,4 +3,11 @@ class Crawler:
         self.get_links_for_url = get_links_for_url
     
     def crawl(self, starting_url):
-        return {starting_url: self.get_links_for_url(starting_url)}
+        urls_to_crawl = [starting_url]
+        result = {}
+        while urls_to_crawl:
+            url = urls_to_crawl.pop(0)
+            links = self.get_links_for_url(url)
+            urls_to_crawl += links
+            result[url] = links
+        return result
--- web-crawlers/python/crawler/test_crawler.py
+++ web-crawlers/python/crawler/test_crawler.py
@@ -1,13 +1,37 @@
+import pytest
 from crawler import Crawler
 
 def test_scrapes_page_with_no_internal_links():
     starting_url = 'http://example.com'
     expected_urls = ['http://another-example.com/example']
-    mock_http_client = lambda _: expected_urls
+    def mock_http_client(url):
+        match url:
+            case 'http://example.com':
+                return expected_urls
+            case _:
+                pytest.fail('Should not fetch external URLs')
 
     crawler = Crawler(mock_http_client)
 
-    result = crawler.crawl(starting_url)  # Dict[Url, List[Url]]
+    result = crawler.crawl(starting_url)
 
     assert starting_url in result
     assert result.get(starting_url) == expected_urls
+
+
+def test_scrapes_page_and_follows_internal_link():
+    starting_url = 'http://example.com'
+
+    def mock_http_client(url):
+        match url:
+            case 'http://example.com':
+                return ['http://example.com/another_page']
+            case 'http://example.com/another_page':
+                return []
+
+    crawler = Crawler(mock_http_client)
+
+    result = crawler.crawl(starting_url)
+
+    assert result.get(starting_url) == ['http://example.com/another_page']
+    assert result.get('http://example.com/another_page') == []
