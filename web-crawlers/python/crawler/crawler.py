2832faef4f2774ee502d0d32068bb6759f25b5a6
Joe Ray <joe@joeray.me>
2024-04-17 20:57:50 +0100

    Don't crawl links that include the exact text of the starting_url

--- web-crawlers/python/crawler/crawler.py
+++ web-crawlers/python/crawler/crawler.py
@@ -8,6 +8,7 @@ class Crawler:
         while urls_to_crawl:
             url = urls_to_crawl.pop(0)
             links = self.get_links_for_url(url)
-            urls_to_crawl += links
+            internal_links = [link for link in links if starting_url in link]
+            urls_to_crawl += internal_links
             result[url] = links
         return result
