e3929ea0a3d83c026937fb52768ae3a5801794bc
Joe Ray <joe@joeray.me>
2024-05-08 22:15:29 +0100

    Create a dirty asyncio implementation

--- web-crawlers/python/crawler/crawler.py
+++ web-crawlers/python/crawler/crawler.py
@@ -1,5 +1,19 @@
 from urllib.parse import urlparse
-from typing import Callable, List, Generator
+from asyncio import gather, run
+from typing import Callable, List, Generator, Coroutine
+
+import aiohttp
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
+
+async def get_links_async(url: str) -> List[str]:
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            print(f"Getting {url} ...")
+            page_text = await response.text()
+            soup = BeautifulSoup(page_text, "html.parser")
+            anchors = soup.find_all("a")
+            return [urljoin(url, anchor.get('href')) for anchor in anchors]
 
 class Crawler:
     def __init__(self, get_links_for_url: Callable[[str], List[str]]) -> None:
@@ -16,12 +30,23 @@ class Crawler:
         urls_to_crawl = [starting_url]
         crawled_urls: set[str] = set()
         while urls_to_crawl:
-            url = urls_to_crawl.pop(0)
-            links = self.get_links_for_url(url)
-            crawled_urls.add(url)
-            internal_links = [link for link in links if self.is_internal_url(link, starting_url)]
-            urls_to_crawl += [link for link in internal_links if link not in crawled_urls]
-            yield (url, links)
+            urls = urls_to_crawl[:10]
+            urls_to_crawl = urls_to_crawl[10:]
+            
+            async def get_urls():
+                results = await gather(*[get_links_async(url) for url in urls])
+                return {url: links for url, links in zip(urls, results)}
+            url_to_links_map = run(get_urls())
+            crawled_urls = crawled_urls.union(urls)
+            
+            unique_links = set(link for links in url_to_links_map.values() for link in links)
+            urls_to_crawl += [link for link in unique_links if link not in crawled_urls and self.is_internal_url(link, starting_url)]
+
+            for url, links in url_to_links_map.items():
+                yield (url, links)
 
     def crawl(self, starting_url: str) -> dict[str, List[str]]:
         return {url: links for url, links in self._crawl(starting_url)}
+    
+
+
